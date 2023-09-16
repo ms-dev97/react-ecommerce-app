78737f5a4142eb21f08a3716c0fa07caec596b01
Mohammed Salem <mohammed.salem701@gmail.com>
2023-09-16 15:08:30 +0300

    add pagination to the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,24 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import ProductCard from "../components/ProductCard";
+import './Home.css';
 import {motion} from 'framer-motion';
+import { Pagination } from "rsuite";
 
 export default function Home() {
-    const [products, setProducts] = useState([]);
+    const [data, setData] = useState({});
     const [isLoading, setIsLoading] = useState(true);
     const [fetchError, setfetchError] = useState(false);
+    const [activePage, setActivePage] = useState(1);
 
     useEffect(() => {
-        fetch('https://dummyjson.com/products')
+        fetch(`https://dummyjson.com/products?limit=10&skip=${(activePage-1)*10}`)
             .then(res => res.json())
             .then(data => {
-                setProducts(data.products);
+                setData(data);
                 setIsLoading(false);
             }).catch(e => setfetchError(true));
-    }, []);
+    }, [activePage]);
 
     return (
         <motion.div
@@ -27,19 +30,30 @@ export default function Home() {
         {isLoading == true ? (
             !fetchError ? <div>Loading</div> : <div>Something went wrong!</div>
         ) : (
-            <div className="container mx-auto">
-                <div className="grid grid-cols-1 md:grid-cols-4 gap-3">
-                    {products.map(product => (
-                        <ProductCard key={product.id}
-                                    id={product.id}
-                                    category={product.category}
-                                    thumbnail={product.thumbnail}
-                                    title={product.title}
-                                    price={product.price}
-                                    discountPercentage={product.discountPercentage} />
-                    ))}
+            <>
+                <div className="container mx-auto">
+                    <div className="grid grid-cols-1 md:grid-cols-4 gap-3">
+                        {data.products.map(product => (
+                            <ProductCard key={product.id}
+                                        id={product.id}
+                                        category={product.category}
+                                        thumbnail={product.thumbnail}
+                                        title={product.title}
+                                        price={product.price}
+                                        discountPercentage={product.discountPercentage} />
+                        ))}
+                    </div>
                 </div>
-            </div>
+
+                <Pagination
+                    total={data.total}
+                    limit={data.limit}
+                    activePage={activePage}
+                    onChangePage={setActivePage}
+                    className="container mx-auto my-10"
+                />
+
+            </>
         )}
         </motion.div>
     );
